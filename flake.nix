4966f7989ecdcc77206593e67f3712d8307f1d38
Stuart Hicks <stuarthicks@users.noreply.github.com>
2022-10-10 10:31:24 +0100

    auto format nix files

--- flake.nix
+++ flake.nix
@@ -5,24 +5,24 @@
     cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
   };
 
-  outputs = inputs: with inputs;
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [cargo2nix.overlays.default];
-        };
+  outputs = inputs:
+    with inputs;
+      flake-utils.lib.eachDefaultSystem (
+        system: let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [cargo2nix.overlays.default];
+          };
 
-        rustPkgs = pkgs.rustBuilder.makePackageSet {
-          rustVersion = "1.61.0";
-          packageFun = import ./Cargo.nix;
-        };
-
-      in rec {
-        packages = {
-          toolkit = (rustPkgs.workspace.toolkit {}).bin;
-          default = packages.toolkit;
-        };
-      }
-    );
+          rustPkgs = pkgs.rustBuilder.makePackageSet {
+            rustVersion = "1.61.0";
+            packageFun = import ./Cargo.nix;
+          };
+        in rec {
+          packages = {
+            toolkit = (rustPkgs.workspace.toolkit {}).bin;
+            default = packages.toolkit;
+          };
+        }
+      );
 }
